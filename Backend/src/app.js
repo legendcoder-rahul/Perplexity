87185c883d8dfa7711389728171e697070ad9a3d
Rahul <rahul251035@gmail.com>
2026-04-04 15:02:05 +0530

    renden deploy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,7 +13,7 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://perplexity-seven-smoky.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
